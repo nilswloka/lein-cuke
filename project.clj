4f42145c02792f2cd05d965eb49ec5df4a02709f
Nils Wloka <nils.wloka@googlemail.com>
2010-06-10 00:28:47 +0200

    Update to latest cuke4duke version and include gherkin in installed gems

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject lein-cuke "0.0.1-SNAPSHOT"
+(defproject lein-cuke "0.0.2-SNAPSHOT"
   :description "Cucumber runner for leiningen"
   :repositories [["cukes" "http://cukes.info/maven"]
                  ["clojars" "http://clojars.org/repo"]]
-  :dependencies [[leiningen "1.0.0-SNAPSHOT"]
-                 [lein-clojars "0.5.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [leiningen "1.0.0"]
+                 [lein-clojars "0.5.0"]
                  [lein-cuke "0.0.1-SNAPSHOT"]
-                 [cuke4duke "0.2.3"]])
+                 [cuke4duke "0.3.0"]])
 
